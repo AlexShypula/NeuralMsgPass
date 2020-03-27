86a2084aab85672c1b3fa27dd09fb1a3666fd2dd
Alex Shypula <Alex@Alexs-MacBook-Pro.local>
2020-03-26 23:13:33 -0400

    add preliminary changes after discussion

--- modules/model.py
+++ modules/model.py
@@ -35,30 +35,34 @@ class my_model(nn.Module):
         self.aggregation_activation = nn.Tanh()
         self.Ws = nn.Linear(self.input_size + 2 * self.hidden_size, self.hidden_size)
         self.Us = nn.Linear(self.hidden_size, 1)
+        self.softmax = nn.Softmax(dim = 1)
 
-    def forward(self, x):
-        padded_input = rnn.pack_sequence(x)
+    def forward(self, x, TASK):
 
-        for task_specific_lstm in self.task_specific_lstm_list:
-            shared_task_output, (h_shared, c_n) = self.share_lstm(padded_input)
-            St = torch.zeros(1)
-            for t in range(h_shared.size(1)):
-                catted_input = torch.cat((padded_input[:, t, :], shared_task_output[:, t, :], shared_task_output), dim=2)
-                for sub_t in range(h_shared.size(1)):
-                    var1 = torch.cat((padded_input[:, sub_t, :], shared_task_output[:, t, :], shared_task_output[:, sub_t, :]), dim=2)
-                    Si= self.Us(self.aggregation_activation(self.Ws(var1)))
-                    Si = torch.cat()
-                beta = nn.Softmax(St)
-                Rt = sum(beta * shared_task_output)
+        padded_input = rnn.pad_sequence(x)
+        embeddings = self.embed(padded_input)
 
+        shared_task_output, (h_shared, c_n) = self.share_lstm(embeddings)
+        h_task = torch.zero(embeddings.size(0), h_shared.size(1), self.hidden_size)
+        state_h = torch.zeros(embeddings.size(0), self.hidden_size)
+        state_c = torch.zeros(embeddings.size(0), self.hidden_size)
 
-                inter_var_1 = self.Ws(catted_input)
-                inter_var_2 = self.aggregation_activation(inter_var_1)
-                Si = self.Us(inter_var_2)
-                if St == 0:
-                    St = Si
-                else:
-                    St = torch.cat((St, Si))
-            Rt = St * h_shared
-            h_task = task_specific_lstm(catted_input, Rt)
+        message_lstm = self.task_specific_lstm_list[TASK]
+        outputs = []
+        # loop for the "decoder" or task specific layer
+        for t in range(h_shared.size(1)):
+            catted_input = torch.cat((h_shared, h_task, embeddings), dim = 2)
+            var1 = self.Ws(catted_input) # var 1 is B x T x H
+            var2 = self.aggregation_activation(var1) # var 1 is B x T x H
+            Si = self.Us(var2) # Si is B x T x 1
+            B = self.softmax(Si) # B is B x T x 1, softmax over T
+            norm_h_shared = torch.mul(B, h_shared) # (B x T x 1 ) x (B x T x H) -> (B x T x H)
+            Rt = torch.sum(norm_h_shared, dim = 1) # (B x H)
+            output, state_h, state_c = self.task_specific_lstm_list[TASK]._step(embeddings[t], Rt, state_h, state_c)
+            outputs.append(output)
+            h_task = state_h.repeat(1, h_shared.size(1), 1) # state_h was B x H -> B x T x H
+
+        out = torch.stack(outputs, axis=1)
+
+        return out
 
